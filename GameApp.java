75e5d2aa20bc036c81de2d856aeed80bd68d6930
Yohdah <Kevindalemercad@gmail.com>
2025-02-18 19:55:40 +0800

    Create GameApp.java

---
+++ GameApp.java
@@ -0,0 +1,19 @@
+public class GameApp {
+    public static void main(String[] args) {
+        Character knight = new Knight();
+        Character wizard = new Wizard();
+        Character archer = new Archer();
+        
+        System.out.println("Knight:");
+        knight.attack();
+        knight.defend();
+        
+        System.out.println("\nWizard:");
+        wizard.attack();
+        wizard.defend();
+        
+        System.out.println("\nArcher:");
+        archer.attack();
+        archer.defend();
+    }
+}
