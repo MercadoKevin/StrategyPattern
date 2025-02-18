692ad68fd54b35dc3ba12a7bd082cab207941916
Yohdah <Kevindalemercad@gmail.com>
2025-02-18 20:03:03 +0800

    Update and rename KnightShield.java to KnightBlock.java

--- KnightBlock.java
+++ KnightBlock.java
@@ -1,4 +1,4 @@
-public class KnightShield implements DefenseStrategy {
+public class KnightBlock implements DefenseStrategy {
     public void defend() {
         System.out.println("Using a shield to defend!");
         System.out.println("Dodging to avoid attack!");
