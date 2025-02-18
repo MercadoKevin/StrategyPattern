f13ed79d6c83ae5a01f7df164f077890964a31f3
Yohdah <Kevindalemercad@gmail.com>
2025-02-18 19:49:40 +0800

    Update ArcherDodge.java

--- ArcherDodge.java
+++ ArcherDodge.java
@@ -1,4 +1,4 @@
-class ArcherDodge implements DefenseStrategy {
+public class ArcherDodge implements DefenseStrategy {
     public void defend() {
         System.out.println("Dodging to avoid attack!");
     }
