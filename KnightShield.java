df047cbd591249eeef3f27757cf9ce14c14a80f0
Yohdah <Kevindalemercad@gmail.com>
2025-02-18 19:57:29 +0800

    Update KnightShield.java

--- KnightShield.java
+++ KnightShield.java
@@ -1,5 +1,7 @@
 public class KnightShield implements DefenseStrategy {
     public void defend() {
         System.out.println("Using a shield to defend!");
+        System.out.println("Dodging to avoid attack!");
+        System.out.println("Casting a magic barrier for defense!");
     }
 }
